d32a159da9b9073c70ab7e20a60f8b125d35d54c
Bony Emmanuel <34760059+opxyc@users.noreply.github.com>
2018-02-25 12:09:30 +0530

    Add files via upload

--- tecaura/js/motions.js
+++ tecaura/js/motions.js
@@ -4,3 +4,31 @@ document.addEventListener('DOMContentLoaded', function() {
 		document.getElementById('toPop').className+=" push-left"; 
 	}
 }, false);
+
+function right(count){
+	var curr=document.getElementById('ws-curr');
+	if(curr.value>=count)
+		return false;
+	var prev=document.getElementById('ws-prev');
+	var current=curr.value;
+	var previous=prev.value;
+	var next=parseInt(current)+1;
+	document.getElementById('ws-slide-'+current).className="huddle-holder outdisplay";
+	document.getElementById('ws-slide-'+next).className="huddle-holder indisplay";
+	curr.value=next;
+	prev.value=current;
+}
+
+function left(count){
+	var curr=document.getElementById('ws-curr');
+	if(curr.value<=count)
+		return false;
+	var prev=document.getElementById('ws-prev');
+	var current=curr.value;
+	var previous=prev.value;
+	var next=parseInt(current)-1;
+	document.getElementById('ws-slide-'+current).className="huddle-holder outdisplay";
+	document.getElementById('ws-slide-'+next).className="huddle-holder indisplay";
+	curr.value=next;
+	prev.value=current;
+}
